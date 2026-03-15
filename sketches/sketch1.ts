79a926265e551eded5fe9382b80cf4228dcc86fe
sashko <320320ua@gmail.com>
2026-03-15 18:23:19 +0200

    added error correction for the spring (25 collisions to 33 collisions in a perfect system)

--- sketches/sketch1.ts
+++ sketches/sketch1.ts
@@ -2,24 +2,32 @@
 // Have a look at the P5 Reference: https://p5js.org/reference/
 // Click the P5 button at the top to run your sketch! ⤴
 
+// =============
 const springConstant = 1;
-const dt = 1e-2;
+const dt = .001; 
 let springX = -350;
-const air_coef = .9999;
-const ground_coef = .9999;
+const air_coef = 1;
+const ground_coef = 1;
 const ballMass = 1;
-const pendulumMass = 2;
+const pendulumMass = 1;
 const pendulumX = 0;
 const pendulumL = 100;
 const g = 9.81;
-const energyConservedDuringCollision = 0.5;
-let shouldCollide = true;
+const energyConservedDuringCollision = 1;
+const speedup = 1000;
 
-let ballState = { v: 0, x: 0 };
-let pendState = { thetaV: 0, theta: PI / 3 };
+let ballState = { v: 0, x: -20 };
+let pendState = { thetaV: 0, theta: PI / 2 };
+// =============
+let collisions = 0;
+let shouldCollide = true;
+let isDeforming = false;
+let savedVelocity = 0;
+
 
 function setup() {
-    resizeCanvas(800, 400);
+    resizeCanvas(800, 400, WEBGL);
+    frameRate(120);
 }
 
 function deform() {
@@ -41,18 +49,30 @@ function drawBallSpring() {
     const ballA = springBallAcceleration();
     ballState.v += ballA * dt;
     ballState.v *= ground_coef;
-    ballState.x += ballState.v;
+    ballState.x += ballState.v * dt;
+    if (deform() < 0 && !isDeforming) {
+        isDeforming = true;
+        savedVelocity = ballState.v;
+        console.log("saved",ballState.v)
+    }
+    if (deform() == 0 && isDeforming) {
+        isDeforming = false;
+        console.log("new",ballState.v);
+        ballState.v = -savedVelocity;
+    }
+
 }
 
 function draw() {
-    background("white");
     translate(width / 2, height / 2);
-    fill("white");
-    drawBallSpring();
-    drawPendulum();
-    ballCollision();
-    // console.log(ballMass*ballState.v + pendulumMass * pendState.thetaV*pendulumL)
-    // console.log(ballMass*ballState.v*ballState.v + pendulumMass*)
+    for(let i=0;i<speedup;i++) {
+        background("white");
+        fill("white");
+        drawBallSpring();
+        drawPendulum();
+        ballCollision();
+        // clear();
+    }
 }
 
 function ballCollision() {
@@ -60,12 +80,12 @@ function ballCollision() {
     const dis = dist(
         ballState.x,
         0,
-        pendulumL * sin(pendState.theta),
+        pendulumX + pendulumL * sin(pendState.theta),
         -pendulumL + pendulumL * cos(pendState.theta),
     );
     if (dis >= 20) shouldCollide = true;
     if (dis <= 20 && shouldCollide) {
-        console.log("collide");
+        console.log("collide", ++collisions);
         shouldCollide = false;
         const e = sqrt(energyConservedDuringCollision);
 
@@ -95,11 +115,13 @@ function drawPendulum() {
     line(
         pendulumX,
         -pendulumL,
-        pendulumL * sin(pendState.theta),
+        pendulumX + pendulumL * sin(pendState.theta),
         -pendulumL + pendulumL * cos(pendState.theta),
     );
 
+    // console.log(pendState.theta);
+
     pendState.thetaV -= g / pendulumL * sin(pendState.theta) * dt;
     pendState.thetaV *= air_coef;
-    pendState.theta += pendState.thetaV;
+    pendState.theta += pendState.thetaV * dt;
 }
